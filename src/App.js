08972c83f8de483191e2a1bb16ec594e8318449e
Scotsquared <didipoopidid98@gmail.com>
2020-12-14 00:07:42 -0600

    initial code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log()
+  return (
+    <div className="App">
+  lol you stink
+    </div>
+  );
+}
+
+export default App;
